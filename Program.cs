2b75ed03685d1246baf18157005b9935b09b85d0
Akash Kumar Sengar <akashsengar55@gmail.com>
2022-03-15 14:26:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace any
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string c = "Ajit";
+           // c = "Akash";
+
+            Console.WriteLine(c[0]);
+          //  c[0] = "b";
+            Console.WriteLine(c[0]);
+            StringBuilder x = new StringBuilder("Ajit");
+            Console.WriteLine(x[0]);
+            x[0] ='b';
+            Console.WriteLine(x);
+
+
+
+
+        }
+    }
+}
